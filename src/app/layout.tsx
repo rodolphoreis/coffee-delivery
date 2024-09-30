e210d8308d4e91dfcc8d4f58fe285a32dff32812
Rodolpho Reis <rodolphoreis@live.com>
2024-09-30 16:16:04 +0100

    feat: add Ballo 2 font with multiple weights and apply to body

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,11 @@
 import type { Metadata } from "next";
-
 import "./globals.css";
+import { Baloo_2 } from "next/font/google";
 
+const ballo2 = Baloo_2({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
 export const metadata: Metadata = {
   title: "Coffee Delivery",
   description: "App about Coffee Delivery",
@@ -14,7 +18,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt">
-      <body>{children}</body>
+      <body
+        className={`${ballo2.className} container mx-auto w-full bg-background`}
+      >
+        <div className="flex md:max-w-[1120px] mx-auto h-screen">
+          {children}
+        </div>
+      </body>
     </html>
   );
 }
