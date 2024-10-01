13851b7c58dd4bbee24be5e51f952ff96a375ba5
Rodolpho Reis <rodolphoreis@live.com>
2024-10-01 15:59:42 +0100

    feat: implement Header component with logo and cart button

---
+++ src/components/Header.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+import Link from "next/link";
+import Cart from "./Cart";
+
+export default function Header() {
+  return (
+    <header className="fixed top-0 left-0 right-0 flex items-center justify-between h-[104px] px-32 py-4 bg-white shadow-md z-50">
+      <div>
+        <Link href="/">
+          <Image src="/Logo.svg" alt="Logo" width={85} height={40}></Image>
+        </Link>
+      </div>
+      <div className="flex gap-3">
+      <button type="button" className="flex p-2 justify-center items-center gap-1 bg-purple-light w-[143px] h-[38px] rounded-xl">
+       <Image src="/location.svg"
+        alt="Location"
+        width={22}
+        height={22}>
+       </Image>
+        <span className="Roboto text-xl font-normal leading-tight ">Portugal</span>
+      </button>
+      <Cart/>
+      </div>
+    </header>
+  );
+}
