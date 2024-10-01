e025939f9fce23785bc6b6027091120cb887a221
Rodolpho Reis <rodolphoreis@live.com>
2024-10-01 15:59:11 +0100

    feat: add ItensInCart component to display item count

---
+++ src/components/ItensInCard.tsx
@@ -0,0 +1,9 @@
+export default function ItensInCart() {
+  return (
+    <div className=" bg-yellow-dark justify-center items-center text-center rounded-full w-[25px] h-[20px] absolute top-[-8px] left-7 pb-6">
+      <span className="text-xs text-white  items-center leading-tight rounded-full w-[25px] h-[25px]">
+        3
+      </span>
+    </div>
+  );
+}
