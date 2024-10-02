d84b553420b539b2e1df04ac48eb93b26c878fa3
Rodolpho Reis <rodolphoreis@live.com>
2024-10-02 17:09:35 +0100

    refactor(header): improve layout structure and spacing for responsiveness

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,28 +4,34 @@ import Cart from "./Cart";
 
 export default function Header() {
   return (
-    <header className="fixed top-0 left-0 right-0 flex items-center justify-between h-[104px] px-40 py-4 z-50 md:max-w-[1440px] mx-auto bg-white">
-      <div>
-        <Link href="/">
-          <Image src="/Logo.svg" alt="Logo" width={85} height={40}></Image>
-        </Link>
-      </div>
-      <div className="flex gap-3">
-        <button
-          type="button"
-          className="flex p-2 justify-center items-center gap-1 bg-purple-light w-[143px] h-[38px] rounded-xl"
-        >
-          <Image
-            src="/location.svg"
-            alt="Location"
-            width={22}
-            height={22}
-          ></Image>
-          <span className="Roboto text-xl font-normal leading-tight ">
-            Portugal
-          </span>
-        </button>
-        <Cart />
+    <header className="fixed top-0 left-0 right-0 flex items-center justify-between h-[104px] px-6 md:px-28 lg:px-20 2xl:px-[310px] py-4 z-50  mx-auto bg-white">
+      <div className=" lg:max-w-[1440px] mx-auto flex justify-between w-full ">
+        <div className="w-16 md:w-20 self-center">
+          <Link href="/">
+            <Image src="/Logo.svg" alt="Logo" width={85} height={40}></Image>
+          </Link>
+        </div>
+        <div className="flex gap-3">
+          <div className="hidden md:block">
+            <button
+              type="button"
+              className="flex px-4 py-2 justify-center items-center gap-1 bg-purple-light w-full rounded-xl"
+            >
+              <Image
+                src="/location.svg"
+                alt="Location"
+                width={22}
+                height={22}
+              ></Image>
+              <span className="text-base Roboto font-normal leading-tight ">
+                Portugal
+              </span>
+            </button>
+          </div>
+          <Link href="/checkout" className="cursor-pointer">
+            <Cart />
+          </Link>
+        </div>
       </div>
     </header>
   );
