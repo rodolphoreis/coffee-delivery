d8573d27c7e97aa1ba140e84b77d77961486f67e
Rodolpho Reis <rodolphoreis@live.com>
2024-10-02 17:08:00 +0100

    refactor: change styles

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -3,8 +3,14 @@ import ItensInCart from "./ItensInCard";
 
 export default function Cart() {
   return (
-    <div className="flex w-[38px] h-[38px] bg-yellow-light rounded-xl relative justify-center">
-      <Image src="/Cart.svg" alt="Cart" width={24} height={24}></Image>
+    <div className="flex w-[33px] h-[33px] md:w-[38px] md:h-[38px] bg-yellow-light rounded-xl relative justify-center self-center">
+      <Image
+        src="/Cart.svg"
+        alt="Cart"
+        width={24}
+        height={24}
+        className="w-[16px] h-[16px] self-center"
+      ></Image>
       <ItensInCart />
     </div>
   );
