8709dd5a3c343f191d9b60c889bb500e03b74373
Rodolpho Reis <rodolphoreis@live.com>
2024-10-03 16:29:27 +0100

    feat: import and render ProductList component in Home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import CoffeeIntro from "@/components/CoffeeIntro";
 import Header from "@/components/Header";
+import ProductList from "@/components/ProductList";
 
 export default function Home() {
   return (
     <div className="flex flex-col">
       <Header />
       <CoffeeIntro />
+      <ProductList />
     </div>
   );
 }
