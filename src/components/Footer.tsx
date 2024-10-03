373595b137befb988e7fb78edf208c8b63a72ae4
Rodolpho Reis <rodolphoreis@live.com>
2024-10-03 17:44:13 +0100

    feat: create Footer component with copyright and author link

---
+++ src/components/Footer.tsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+
+export default function Footer() {
+  return (
+    <footer className="mt-14 text-zinc-500  py-4 font-bold">
+      <p className="text-center">
+        © 2024 Coffee Delivery - Criado por{" "}
+        <Link
+          href="https://www.linkedin.com/feed/"
+          className="text-yellow-600 font-bold hover:underline"
+          target="_blank"
+        >
+          Rodolpho Reis
+        </Link>
+      </p>
+    </footer>
+  );
+}
