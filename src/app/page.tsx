7f078c7a1a11953eb11108761aa4364e527a96db
Rodolpho Reis <rodolphoreis@live.com>
2024-10-03 17:43:51 +0100

    feat: add Footer and set min-h-screen on Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import CoffeeIntro from "@/components/CoffeeIntro";
+import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import ProductList from "@/components/ProductList";
 import { Baloo_2 } from "next/font/google";
@@ -10,15 +11,16 @@ const baloo2 = Baloo_2({
 
 export default function Home() {
   return (
-    <div className="flex flex-col">
+    <div className=" min-h-screen ">
       <Header />
       <CoffeeIntro />
       <h2
-        className={`${baloo2.className} text-xl mb-2 mt-8 ml-3 xl:text-3xl xl:mb-8 font-extrabold leading-9`}
+        className={`${baloo2.className} text-xl mb-2 mt-8 ml-3  xl:text-3xl xl:mb-8 font-extrabold leading-9`}
       >
         Nossos cafés
       </h2>
       <ProductList />
+      <Footer />
     </div>
   );
 }
