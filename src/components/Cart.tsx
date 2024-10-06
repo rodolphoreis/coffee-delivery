09627ddebfdc62c008076ef858b3d28f8b69fcca
Rodolpho Reis <rodolphoreis@live.com>
2024-10-06 20:31:40 +0100

    feat(cart): calculate total items in cart using context

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,7 +1,22 @@
+"use client";
 import Image from "next/image";
 import ItensInCart from "./ItensInCard";
+import { useContext } from "react";
+import { ItensContext } from "../../context/ItensContext";
 
 export default function Cart() {
+  const context = useContext(ItensContext);
+
+  if (!context) {
+    throw new Error("ItensInCart deve ser usado dentro de um ItensProvider");
+  }
+
+  const { itemQuantities } = context;
+
+  const totalItems = Object.values(itemQuantities).reduce(
+    (acc, quantity) => acc + quantity,
+    0
+  );
   return (
     <div className="flex w-[33px] h-[33px] md:w-[38px] md:h-[38px] bg-yellow-light rounded-xl relative justify-center self-center">
       <Image
@@ -11,7 +26,7 @@ export default function Cart() {
         height={24}
         className="w-[16px] h-[16px] self-center"
       ></Image>
-      <ItensInCart />
+      {totalItems > 0 && <ItensInCart />}
     </div>
   );
 }
