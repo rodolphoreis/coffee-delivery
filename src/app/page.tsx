2c680a75ba1ed966b2a96010a10eee02580ee112
Rodolpho Reis <rodolphoreis@live.com>
2024-10-06 20:29:17 +0100

    feat(context): wrap main components with ItensContextProvider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import ProductList from "@/components/ProductList";
 import { Baloo_2 } from "next/font/google";
+import ItensContextProvider from "../../context/ItensContext";
 
 const baloo2 = Baloo_2({
   subsets: ["latin"],
@@ -12,15 +13,17 @@ const baloo2 = Baloo_2({
 export default function Home() {
   return (
     <div className=" min-h-screen ">
-      <Header />
-      <CoffeeIntro />
-      <h2
-        className={`${baloo2.className} text-xl mb-2 mt-8 ml-3  xl:text-3xl xl:mb-8 font-extrabold leading-9`}
-      >
-        Nossos cafés
-      </h2>
-      <ProductList />
-      <Footer />
+      <ItensContextProvider>
+        <Header />
+        <CoffeeIntro />
+        <h2
+          className={`${baloo2.className} text-xl mb-2 mt-8 ml-3  xl:text-3xl xl:mb-8 font-extrabold leading-9`}
+        >
+          Nossos cafés
+        </h2>
+        <ProductList />
+        <Footer />
+      </ItensContextProvider>
     </div>
   );
 }
