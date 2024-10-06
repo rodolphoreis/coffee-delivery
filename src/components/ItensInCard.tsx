de28104aff756378169b199d46b81b95972614fa
Rodolpho Reis <rodolphoreis@live.com>
2024-10-06 20:30:53 +0100

    feat(cart): display total items in cart using ItensContext

--- src/components/ItensInCard.tsx
+++ src/components/ItensInCard.tsx
@@ -1,8 +1,24 @@
+"use client";
+import React, { useContext } from "react";
+import { ItensContext } from "../../context/ItensContext";
+
 export default function ItensInCart() {
+  const context = useContext(ItensContext);
+
+  if (!context) {
+    throw new Error("ItensInCart deve ser usado dentro de um ItensProvider");
+  }
+
+  const { itemQuantities } = context;
+
+  const totalItems = Object.values(itemQuantities).reduce(
+    (acc, quantity) => acc + quantity,
+    0
+  );
   return (
     <div className=" bg-yellow-dark justify-center items-center text-center rounded-full px-2 py-1   absolute top-[-6px] left-6 sm:top-[-8px] sm:left-6  ">
       <span className="flex items-center justify-center text-xs text-white  leading-tight ">
-        12
+        {totalItems}
       </span>
     </div>
   );
