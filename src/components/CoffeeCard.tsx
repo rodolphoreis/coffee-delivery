2e881f369a66f09592ce77d9113fb9a189ad3f67
Rodolpho Reis <rodolphoreis@live.com>
2024-10-08 18:59:49 +0100

    feat(component): implement CoffeeCard to display product details and manage quantities

--- src/components/CoffeeCard.tsx
+++ src/components/CoffeeCard.tsx
@@ -7,6 +7,15 @@ import CartItemCounter from "./CartItemCounter";
 interface CoffeeCardProps {
   id: number;
 }
+export function CoffeeCard({ id }: CoffeeCardProps) {
+  const { itemQuantities, products, removeItem } = useContext(ItensContext);
+
+  const product = products.find((product) => product.id === id);
+  const quantity = itemQuantities[id] || 0;
+
+  if (!product || quantity === 0) {
+    return null;
+  }
 
   return (
     <div className="flex  px-1 py-2 border-b justify-between border-b-zinc-300 pb- md:pb-8 mb-4">
