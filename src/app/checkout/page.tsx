b830f20c67ee0cffc303b5707345a83ba0780b83
Rodolpho Reis <rodolphoreis@live.com>
2024-10-08 18:56:17 +0100

    feat(context): wrap application with ItensContextProvider for state management

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -1,17 +1,20 @@
 import Form from "@/components/Form";
 import Header from "@/components/Header";
+import ItensContextProvider from "../../../context/ItensContext";
 
 export default function Checkout() {
   return (
-    <div className="flex flex-col min-h-screen">
-      <header>
-        <Header />
-      </header>
-      <main className="flex-1">
-        <section className="mt-44">
-          <Form />
-        </section>
-      </main>
-    </div>
+    <ItensContextProvider>
+      <div className="flex flex-col min-h-screen">
+        <header>
+          <Header />
+        </header>
+        <main className="flex-1">
+          <section className="mt-44">
+            <Form />
+          </section>
+        </main>
+      </div>
+    </ItensContextProvider>
   );
 }
