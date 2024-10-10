0a6e7097f04bd4fadc8d4a28dbd4fb491168b82e
Rodolpho Reis <rodolphoreis@live.com>
2024-10-10 15:24:02 +0100

    style: adjust Tailwind CSS classes for responsive text and padding in button layout

--- src/components/CoffeeCard.tsx
+++ src/components/CoffeeCard.tsx
@@ -20,23 +20,24 @@ export function CoffeeCard({ id }: CoffeeCardProps) {
 
   return (
     <div className="flex px-1 py-2 border-b justify-between border-b-zinc-300 pb- md:pb-8 mb-4">
-      <div className="flex gap-5">
+      <div className="flex gap-2 sm:gap-5">
         <div>
           <Image
             src={product.image}
             alt={product.product}
             width={74}
             height={74}
+            className=" w-[64px] h-[56px] sm:w-[54px] sm:h-[36px]"
           />
         </div>
         <div className="flex flex-col gap-2">
           <h1>{product.product}</h1>
-          <div className="flex gap-3">
+          <div className="flex gap-1 sm:gap3">
             <CartItemCounter id={id} />
             <button
               type="button"
               onClick={() => removeItem(id)}
-              className="flex items-center bg-base-hover px-4 py-2 rounded-xl"
+              className="flex text-[12px] sm:text-base items-center bg-base-hover px-2 sm:px-4 py-1 sm:py-2 rounded-xl"
             >
               <Image
                 src="/lixoIcon.svg"
